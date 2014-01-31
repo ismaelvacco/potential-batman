7f194229d352756f4a2b6fa33f1eb94c2761ad85
Ismael Vacco <isma.vacco@gmail.com>
2014-01-31 19:02:09 -0200

    added .gitconfig global

--- install.sh
+++ install.sh
@@ -6,9 +6,6 @@ git clone git@github.com:ismaelvacco/potential-batman.git ~/.potential-batman
 # git config
 rm ~/.gitconfig
 ln -s ~/.potential-batman/.gitconfig ~/.gitconfig
-rm ~/.global_gitignore
-ln -s ~/.potential-batman/.global_gitignore ~/.global_gitignore
-
 
 # vim config
 rm ~/.vimrc
